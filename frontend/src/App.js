addf4dfe1ef35d6434b6c4967ee168916498ed3d
jesuscruz12 <20221059@uthh.edu.mx>
2024-10-18 22:46:29 -0600

    Combinando cambios de App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importa Bootstrap para estilos
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';  
-=======
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';  // Importa Routes y Route correctamente
->>>>>>> 57a9d654f4988941b0210ad3a7eeb1928c37a2cf
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -12,14 +8,10 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import AdminDashboard from './pages/AdminDashboard';
-<<<<<<< HEAD
-import PolicyViewer from './components/PolicyViewer'; // Asegúrate de que este componente esté correctamente implementado
-=======
+import PolicyViewer from './components/PolicyViewer'; // Importa el componente para ver políticas
 import ForgotPassword from './pages/ForgotPassword';
 import ResetPassword from './pages/ResetPassword';
 
-
->>>>>>> 57a9d654f4988941b0210ad3a7eeb1928c37a2cf
 import './App.css';  
 
 function App() {
@@ -34,13 +26,9 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/admin-dashboard" element={<AdminDashboard />} />
-<<<<<<< HEAD
-            {/* Rutas para políticas en modo lectura usando parámetros dinámicos */}
             <Route path="/politicas/:id" element={<PolicyViewer />} />
-=======
             <Route path="/forgot-password" element={<ForgotPassword />} />
             <Route path="/reset-password" element={<ResetPassword />} />
->>>>>>> 57a9d654f4988941b0210ad3a7eeb1928c37a2cf
           </Routes>
         </main>
         <Footer />
